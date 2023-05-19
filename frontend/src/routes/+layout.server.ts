e41f7634d22997743651c8c001c1e76b79e2956a
MM20 <15646950+MM2-0@users.noreply.github.com>
2023-05-19 21:53:15 +0200

    Fix colors and HTML title

--- frontend/src/routes/+layout.server.ts
+++ frontend/src/routes/+layout.server.ts
@@ -1,16 +1,10 @@
 import type { ServerLoadEvent } from '@sveltejs/kit'
 import type { FDroidIndexV2 } from '../lib/fdroid/fdroid-index'
-import { FDROID_FINGERPRINT } from '$env/static/private'
 
 export async function load({ fetch }: ServerLoadEvent) {
 	const data = (await (await fetch('/data')).json()) as FDroidIndexV2
 	return {
 		repo: {
-			description: data.repo.description['en-US'],
-			url: data.repo.address,
-			fingerprint: FDROID_FINGERPRINT,
-			icon: data.repo.address + data.repo.icon['en-US'].name,
-			qrCode: data.repo.address + '/index.png',
 			name: data.repo.name['en-US'],
 		},
 	}
--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,1 +1,10 @@
+<script lang="ts">
+	import type { PageData } from './$types'
+
+	export let data: PageData
+</script>
+
+<svelte:head>
+	<title>{data.repo.name}</title>
+</svelte:head>
 <slot />
