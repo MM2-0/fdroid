04bcbdee978a01f17f2881244c2dfd39a23ffcbe
MM20 <15646950+MM2-0@users.noreply.github.com>
2024-09-03 17:30:48 +0200

    Revert "Update setup.sh"
    This reverts commit 5e89f64f6c85bef5982a538306cec8ed75b2d907.

--- repo/scripts/setup.sh
+++ repo/scripts/setup.sh
@@ -8,7 +8,7 @@ cd tmp
 
 python -m venv venv
 source venv/bin/activate
-pip install "fdroidserver"
+pip install "fdroidserver" "androguard < 4"
 fdroid init
 mkdir metadata
 echo $FDROID_KEYSTORE_BASE64 | base64 -di > "keystore.p12"
